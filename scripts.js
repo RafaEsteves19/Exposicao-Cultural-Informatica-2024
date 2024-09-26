672608805a8d767490d7895b062c0d187590827b
RafaEsteves19 <rafaeldev019@gmail.com>
2024-09-26 14:38:55 -0300

    fifth commit

--- scripts.js
+++ scripts.js
@@ -1,14 +1,28 @@
-
-
 document.querySelectorAll('nav a').forEach(anchor => {
     anchor.addEventListener('click', function(e) {
         e.preventDefault();
         
         const targetId = this.getAttribute('href');
         const targetElement = document.querySelector(targetId);
-        
-        targetElement.scrollIntoView({
-            behavior: 'smooth'
-        });
+
+        if (targetElement) {
+            const elementHeight = targetElement.offsetHeight;
+            const windowHeight = window.innerHeight;
+            const targetPosition = targetElement.getBoundingClientRect().top + window.scrollY - (windowHeight / 2) + (elementHeight / 2);
+
+            window.scrollTo({
+                top: targetPosition,
+                behavior: 'smooth'
+            });
+
+            // Oculta a navbar após o clique
+            const navbar = document.getElementById("navbar");
+            navbar.classList.add("hidden");
+        }
     });
+});
+
+document.getElementById("menu-button").addEventListener("click", function() {
+    const navbar = document.getElementById("navbar");
+    navbar.classList.toggle("hidden");
 });
